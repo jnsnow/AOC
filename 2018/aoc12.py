a353dc622a0655a41f2ecbad1de3f7c610d8093b
jnsnow <john.n.snow@gmail.com>
2018-12-12 01:55:23 -0500

    refactor aoc12

--- 2018/aoc12.py
+++ 2018/aoc12.py
@@ -10,6 +10,33 @@ class rule:
         self.pattern = tmp[0]
         self.result = tmp[1].strip()
 
+class simulation:
+    def __init__(self, state, rules):
+        # find rules that produce a plant, the others are safe to ignore
+        self.rules = [r for r in rules if r.result == '#']
+        # Implement as a sparse dict, as is tradition
+        self.state = defaultdict(lambda: '.')
+        for i in range(len(state)):
+            self.state[i] = state[i]
+
+    def tick(self):
+        # Rules need a lookback/lookahead of +-2
+        low = min(self.state.keys()) - 2
+        high = max(self.state.keys()) + 2
+        nextgen = defaultdict(lambda: '.')
+        for k in range(low, high + 1):
+            for r in self.rules:
+                if (self.state[k-2] == r.pattern[0] and
+                    self.state[k-1] == r.pattern[1] and
+                    self.state[k] == r.pattern[2] and
+                    self.state[k+1] == r.pattern[3] and
+                    self.state[k+2] == r.pattern[4]):
+                        nextgen[k] = '#'
+        self.state = nextgen
+
+    def value(self):
+        return sum(self.state.keys())
+
 def load(filename):
     f = open("/home/nago/Documents/input12.txt", "r")
     line = f.readline()
@@ -19,29 +46,10 @@ def load(filename):
     return (state, rules)
 
 def p1(state, rules):
-    # find rules that produce a plant, the others are safe to ignore
-    prodrules = [r for r in rules if r.result == '#']
-
-    # Implement as a sparse dict, as is tradition
-    sim = defaultdict(lambda: '.')
-    for i in range(len(state)):
-        sim[i] = state[i]
-
+    sim = simulation(state, rules)
     for generation in range(1, 20 + 1):
-        # Rules need a lookback/lookahead of +-2
-        low = min(sim.keys()) - 2
-        high = max(sim.keys()) + 2
-        nextgen = defaultdict(lambda: '.')
-        for k in range(low, high + 1):
-            for r in prodrules:
-                if (sim[k-2] == r.pattern[0] and
-                    sim[k-1] == r.pattern[1] and
-                    sim[k] == r.pattern[2] and
-                    sim[k+1] == r.pattern[3] and
-                    sim[k+2] == r.pattern[4]):
-                        nextgen[k] = '#'
-        sim = nextgen
-    return sum(sim.keys())
+        sim.tick()
+    return sim.value()
 
 def aoc12(filename):
     state, rules = load(filename)
